9d504cf774d5f65ab155a4c8693042c52b92ca24
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-12 14:45:54 -0500

    Create function TodoContainer

---
+++ src/Components/ToDoContainer/ToDoApp.js
@@ -0,0 +1,11 @@
+import './ToDoApp.css';
+
+const ToDoContainer = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello there! Nice to meet you</p>
+    </header>
+  </div>
+);
+
+export default ToDoContainer;
